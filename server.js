ac15212677e796cdda26f5880f8742cfbfd6280e
Ernest Wong <ewon521@gmail.com>
2017-02-12 11:01:13 +1300

    Generic Bugfix n Tuning
    - Replaced the serial port reading with piped input
    - Updated tank-style drive control
    - Added background image
    - Added scale slider
    - Fix joystick reading bugs

--- server.js
+++ server.js
@@ -1,7 +1,10 @@
 var app = require('http').createServer(handler)
 var io = require('socket.io')(app);
 var SerialPort = require('serialport');
+var readline = require('readline');
 var fs = require('fs');
+
+var rl = readline.createInterface({ input: process.stdin });
 
 app.listen(80);
 console.log('server listening at port 80');
@@ -20,6 +23,31 @@ function handler (req, res) {
 
 io.on('connection', function (socket) {
   console.log('socket connected');
+  function pushdata(data) {
+    process.stdout.write('data: '+ data + '\r');
+    data = data.split(' ').map((x) => parseInt(x));
+    var joyData = {
+        ch1: data[0],
+        ch2: data[1],
+        ch3: data[2],
+        ch4: data[3],
+      btn5U: data[4],
+      btn5D: data[5],
+      btn6U: data[6],
+      btn6D: data[7],
+      btn7U: data[8],
+      btn7D: data[9],
+      btn7L: data[10],
+      btn7R: data[11],
+      btn8U: data[12],
+      btn8D: data[13],
+      btn8L: data[14],
+      btn8R: data[15]
+    };
+    socket.emit('joydata', joyData);
+  }
+  rl.on('line', pushdata);
+  /*
   SerialPort.list(function (err, ports) {
     if (err) {
       console.log(err);
@@ -29,10 +57,10 @@ io.on('connection', function (socket) {
       return;
     }
     var port = new SerialPort(ports[0].comName, {
-      parser: SerialPort.parsers.readline('\n')
+      parser: SerialPort.parsers.readline('\r\n')
     });
     port.on('open', function() {
-      console.log('port opened');
+      console.log('port opened:', ports[0].comName);
     });
     port.on('disconnect', function() {
       console.log('port disconnected');
@@ -40,26 +68,7 @@ io.on('connection', function (socket) {
     port.on('close', function() {
       console.log('port closed');
     });
-    port.on('data', function (data) {
-      var joyData = {
-          ch1: data[0],
-          ch2: data[1],
-          ch3: data[2],
-          ch4: data[3],
-        btn5U: data[4],
-        btn5D: data[6],
-        btn6U: data[7],
-        btn6D: data[8],
-        btn7U: data[9],
-        btn7D: data[10],
-        btn7L: data[11],
-        btn7R: data[12],
-        btn8U: data[13],
-        btn8D: data[14],
-        btn8L: data[15],
-        btn8R: data[16]
-      };
-      socket.emit('joydata', joyData);
-    });
+    port.on('data', pushdata);
   });
+  */
 });
